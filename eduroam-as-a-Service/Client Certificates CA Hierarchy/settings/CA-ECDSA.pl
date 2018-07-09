5f648e7ce44d42599cd4add15ecfc44cd70c1b89
Stefan Winter <stefan.winter@restena.lu>
2018-07-09 14:40:30 +0200

    prep for ECDSA roots in parallel with RSA

--- eduroam-as-a-Service/Client Certificates CA Hierarchy/settings/CA-ECDSA.pl
+++ eduroam-as-a-Service/Client Certificates CA Hierarchy/settings/CA-ECDSA.pl
@@ -53,7 +53,7 @@ $VERIFY="$openssl verify";
 $X509="$openssl x509";
 $PKCS12="$openssl pkcs12";
 
-$CATOP="./ROOT-CA";
+$CATOP="./ROOT-ECDSA";
 $CAKEY="cakey.pem";
 $CAREQ="careq.pem";
 $CACERT="cacert.pem";
