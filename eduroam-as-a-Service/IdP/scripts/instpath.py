e1a3d0e233cf12687d50141aa0d7709b2f827abc
Maja Gorecka-Wolniewicz <mgw@umk.pl>
2018-07-11 08:42:55 +0200

    exception handling added

--- eduroam-as-a-Service/IdP/scripts/instpath.py
+++ eduroam-as-a-Service/IdP/scripts/instpath.py
@@ -11,16 +11,35 @@ if install == 'quit':
     sys.exit(0)
 if not install.endswith('/'):
         install = install + '/'
+cnt = 0;
 for fname in ['sb.py', 'addnro.py', 'delnro.py', 'makelist.py']:
     if os.path.isfile(prefix + fname):
+       cnt = cnt + 1
        with open(prefix + fname, 'r') as f:
-           fw = open(tmp + fname, 'w')
+           try:
+               fw = open(tmp + fname, 'w')
+               cnt = cnt + 1
+           except:
+               print 'ERROR: can not open for writing', tmp + fname    
+               continue
            for row in f:
                if 'FR_INSTALLATION_DIRECTORY' in row:
                    fw.write(row.replace('FR_INSTALLATION_DIRECTORY', install))
                else:
                    fw.write(row)
            fw.close()
-           move(tmp + fname, prefix + fname)
+           try:
+               move(tmp + fname, prefix + fname)
+               cnt = cnt + 1
+           except IOError:
+               print 'ERROR:', 'move ' + tmp + fname, 'to', prefix + fname, 'failed'
+               continue
            os.chmod(prefix + fname, 0755)
+    else:
+        print 'ERROR: ' + prefix + fname, 'not found'
+if cnt == 12:
+    print 'Success!'
+else:
+    print 'Check errors'
+   
 
